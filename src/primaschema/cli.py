cc7089359cd7dd722443e89a2dfb926154df1512
Bede Constantinides <1007616+bede@users.noreply.github.com>
2022-09-08 20:12:35 +0100

    Add back reference checksum

--- src/primaschema/cli.py
+++ src/primaschema/cli.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 import defopt
 
+from Bio import SeqIO
+
 
 import pandas as pd
 
 
-def build(bed_path: Path):
+def hash_bed(bed_path: Path):
     bed_fields = ['chrom', 'chromStart', 'chromEnd', 'name', 'poolName', 'orientation']
     fields_to_hash = ['chrom', 'chromStart', 'chromEnd', 'orientation']
 
@@ -31,18 +33,31 @@ def build(bed_path: Path):
     df_norm = df_norm[[*fields_to_hash]].sort_values('chromStart')  # Sort by start pos
     df_norm_text = df_norm.to_csv(sep='\t', header=False, index=False)  
 
-    hexdigest = hashlib.md5(df_norm_text.encode()).hexdigest()
+    hex_digest = hashlib.md5(df_norm_text.encode()).hexdigest()
 
-    print('Primer scheme hash:', file=sys.stderr)
-    print(hexdigest[:16])  # Use first 64 bits only
-    print('Hash function input:', df_norm_text.strip(), sep='\n', file=sys.stderr)
+    print('BED checksum:', file=sys.stderr)
+    print(hex_digest)
+    return hex_digest
+    # print('Hash function input:', df_norm_text.strip(), sep='\n', file=sys.stderr)
 
 
+def hash_ref(ref_path: Path):
+    record = SeqIO.read(ref_path, "fasta")
+    hex_digest = hashlib.md5(str(record.seq).upper().encode()).hexdigest()
+    print('Reference checksum:', file=sys.stderr)
+    print(hex_digest)
+    return hex_digest
+
+
+def build():
+    pass
 
 
 def main():
     defopt.run(
         {
+            "hash-bed": hash_bed,
+            "hash-ref": hash_ref,
             "build": build,
         },
         no_negated_flags=True,
