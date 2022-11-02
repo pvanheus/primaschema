d1ad0fa656be8fe22c96078b8935893a732cfe0b
Bede Constantinides <1007616+bede@users.noreply.github.com>
2022-11-02 20:33:39 +0000

    Reorganisation

--- src/primaschema/__init__.py
+++ src/primaschema/__init__.py
@@ -1,7 +1,6 @@
-import importlib.metadata
 from pathlib import Path
 
-__version__ = importlib.metadata.version("primaschema")
+__version__ = "0.2.0"
 
 pkg_dir = Path(__file__)
 data_dir = pkg_dir.parent / "data"
--- src/primaschema/cli.py
+++ src/primaschema/cli.py
@@ -1,52 +1,21 @@
 import sys
-import hashlib
-from pathlib import Path
-
 import defopt
 
-from Bio import SeqIO
+from pathlib import Path
 
-
-import pandas as pd
+import primaschema.lib as lib
 
 
 def hash_bed(bed_path: Path):
-    bed_fields = ["chrom", "chromStart", "chromEnd", "name", "poolName", "orientation"]
-    fields_to_hash = ["chrom", "chromStart", "chromEnd", "orientation"]
-
-    df = pd.read_csv(
-        bed_path,
-        sep="\t",
-        names=bed_fields,
-        dtype=dict(
-            chrom=str,
-            chromStart=int,
-            chromEnd=int,
-            name=str,
-            poolName=str,
-            orientation=str,
-        ),
-    )
-
-    # Normalise and hash bed records
-    df_norm = df.applymap(
-        lambda x: x.strip() if isinstance(x, str) else x
-    )  # Strip trailing and leading whitespace
-    df_norm = df_norm[[*fields_to_hash]].sort_values("chromStart")  # Sort by start pos
-    df_norm_text = df_norm.to_csv(sep="\t", header=False, index=False)
-    hex_digest = hashlib.md5(df_norm_text.encode()).hexdigest()
+    hex_digest = lib.hash_bed(bed_path)
     print("BED checksum:", file=sys.stderr)
     print(hex_digest)
-    # print('Hash function input:', df_norm_text.strip(), sep='\n', file=sys.stderr)
-    return hex_digest
 
 
 def hash_ref(ref_path: Path):
-    record = SeqIO.read(ref_path, "fasta")
-    hex_digest = hashlib.md5(str(record.seq).upper().encode()).hexdigest()
+    hex_digest = lib.hash_ref(ref_path)
     print("Reference checksum:", file=sys.stderr)
     print(hex_digest)
-    return hex_digest
 
 
 def build():
