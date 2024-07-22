7da2fdac64c51451616dbd0512a446805b2ffab4
Peter van Heusden <pvanheusden@uwc.ac.za>
2024-07-22 16:14:59 +0200

    Change to use of PRIMER_SCHEMA_DIR environment variable

--- src/primaschema/__init__.py
+++ src/primaschema/__init__.py
@@ -9,8 +9,8 @@ from pathlib import Path
 
 __version__ = "0.2.0"
 
-pkg_dir = os.getenv('PRIMER_SCHEMES', Path(__file__).resolve().parent.parent.parent)
-schema_dir = pkg_dir / "schema"
+pkg_dir = Path(__file__).resolve().parent.parent.parent
+schema_dir = os.environ.get('PRIMER_SCHEMA_DIR', pkg_dir / "schema")
 primer_scheme_schema_path = schema_dir / "primer-scheme_schema.yml"
 manifest_schema_path = schema_dir / "manifest_schema.json"
 header_path = schema_dir / "header.yml"
